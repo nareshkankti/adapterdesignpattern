553a6fee5639fcc1a1a36db104ca3895367c4317
nareshkankti <nareshkankati@gmail.com>
2024-10-24 10:51:19 +0530

    first commit

---
+++ src/main/java/com/vishwamtech/adapterdesignpattern/AdapterdesignpatternApplication.java
@@ -0,0 +1,13 @@
+package com.vishwamtech.adapterdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterdesignpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterdesignpatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishwamtech/adapterdesignpattern/AdapterdesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwamtech.adapterdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterdesignpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
